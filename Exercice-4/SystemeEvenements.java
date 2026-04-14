d3d71965df0b07c82c3e5924a5ac42f957ba0549
Shoko <a.breyton.pro@gmail.com>
2026-04-14 17:08:00 +0200

    Exo 4: ajoute la version lambda et removeListener

--- Exercice-4/SystemeEvenements.java
+++ Exercice-4/SystemeEvenements.java
@@ -49,4 +49,42 @@ public class SystemeEvenements {
         btn.simulerTouche('b');
         btn.simulerSurvol(false);
     }
+
+    public static void mainLambda(String[] args) {
+        Bouton btn = new Bouton("Valider");
+
+        final int[] compteurClics = {0};
+
+        btn.addClickListener((x, y) -> System.out.println("Clic aux coordonnees : (" + x + ", " + y + ")"));
+
+        ClickListener compteurListener = (x, y) -> {
+            compteurClics[0]++;
+            System.out.println("Compteur de clics : " + compteurClics[0]);
+        };
+        btn.addClickListener(compteurListener);
+
+        btn.addKeyListener(touche -> {
+            char min = Character.toLowerCase(touche);
+            if (min == 'a' || min == 'e' || min == 'i' || min == 'o' || min == 'u' || min == 'y') {
+                System.out.println("Touche '" + touche + "' = voyelle");
+            } else {
+                System.out.println("Touche '" + touche + "' = consonne");
+            }
+        });
+
+        btn.addHoverListener(entre -> {
+            if (entre) {
+                System.out.println("Info-bulle: Cliquez pour valider le formulaire");
+            } else {
+                System.out.println("Info-bulle cachee");
+            }
+        });
+
+        btn.simulerSurvol(true);
+        btn.simulerClic(5, 8);
+        btn.simulerTouche('e');
+
+        btn.removeClickListener(compteurListener);
+        btn.simulerClic(99, 99);
+    }
 }
