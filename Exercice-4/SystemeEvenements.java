aa0d7ca1cab8a92bf7e30fc4463bb98c1cbc1b2f
Shoko <a.breyton.pro@gmail.com>
2026-04-14 16:21:00 +0200

    Exo 4: ajoute la version classes anonymes

---
+++ Exercice-4/SystemeEvenements.java
@@ -0,0 +1,52 @@
+public class SystemeEvenements {
+    public static void main(String[] args) {
+        Bouton btn = new Bouton("Valider");
+
+        final int[] compteurClics = {0};
+
+        btn.addClickListener(new ClickListener() {
+            @Override
+            public void onClick(int x, int y) {
+                System.out.println("Clic aux coordonnees : (" + x + ", " + y + ")");
+            }
+        });
+
+        btn.addClickListener(new ClickListener() {
+            @Override
+            public void onClick(int x, int y) {
+                compteurClics[0]++;
+                System.out.println("Compteur de clics : " + compteurClics[0]);
+            }
+        });
+
+        btn.addKeyListener(new KeyListener() {
+            @Override
+            public void onKeyPress(char touche) {
+                char min = Character.toLowerCase(touche);
+                if (min == 'a' || min == 'e' || min == 'i' || min == 'o' || min == 'u' || min == 'y') {
+                    System.out.println("Touche '" + touche + "' = voyelle");
+                } else {
+                    System.out.println("Touche '" + touche + "' = consonne");
+                }
+            }
+        });
+
+        btn.addHoverListener(new HoverListener() {
+            @Override
+            public void onHover(boolean entre) {
+                if (entre) {
+                    System.out.println("Info-bulle: Cliquez pour valider le formulaire");
+                } else {
+                    System.out.println("Info-bulle cachee");
+                }
+            }
+        });
+
+        btn.simulerSurvol(true);
+        btn.simulerClic(10, 20);
+        btn.simulerClic(30, 45);
+        btn.simulerTouche('a');
+        btn.simulerTouche('b');
+        btn.simulerSurvol(false);
+    }
+}
